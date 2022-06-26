31c6a591e494ea580edc14985a1457bcfb69137e
john_summer <john.midsummer.ac@gmail.com>
2022-06-26 21:23:18 +0900

    fix: 動作確認のコードを少し増やした

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ from training import LongTrading
 
 if __name__ == "__main__":
     lt = LongTrading.LongTrading()
-    #result = lt.buy(total_amount_now=300000.0, number_now=2000, buying_number=1000, stock_price=1600.0)
     lt.buy(1000, 1600)
     print("amount: " + str(lt.total_amount_now))
     print("number: " + str(lt.number_now))
@@ -22,3 +21,29 @@ if __name__ == "__main__":
     print("number: " + str(lt.number_now))
     print("profit: " + str(result[2]))
     # print("avg_price: " + str(lt.total_amount_now / lt.number_now))
+
+    print("---------")
+
+    lt = LongTrading.LongTrading()
+    lt.buy(1000, 1600)
+    print("amount: " + str(lt.total_amount_now))
+    print("number: " + str(lt.number_now))
+
+    lt.buy(1000, 2000)
+    print("amount: " + str(lt.total_amount_now))
+    print("number: " + str(lt.number_now))
+
+    result = lt.sell(500, 2500)
+    print("amount: " + str(lt.total_amount_now))
+    print("number: " + str(lt.number_now))
+    print("profit: " + str(result[2]))
+    print("avg_price: " + str(lt.total_amount_now / lt.number_now))
+
+    lt.buy(1000, 2000)
+    print("amount: " + str(lt.total_amount_now))
+    print("number: " + str(lt.number_now))
+
+    result = lt.sell(2500, 2000)
+    print("amount: " + str(lt.total_amount_now))
+    print("number: " + str(lt.number_now))
+    print("profit: " + str(result[2]))
