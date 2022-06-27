b5f190b95ab6d15c5744f729caa9617d01e0f037
john_summer <john.midsummer.ac@gmail.com>
2022-06-27 20:57:11 +0900

    add: STASS-30 ショートトレードの操作を含めたクラスを追加する

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 from training import LongTrading
+from training import ShortTrading
 
-if __name__ == "__main__":
+def long_trading_test():
+
+    print("long_trading_test")
+
     lt = LongTrading.LongTrading()
     lt.buy(1000, 1600)
     print("amount: " + str(lt.total_amount_now))
@@ -22,7 +26,7 @@ if __name__ == "__main__":
     print("profit: " + str(result[2]))
     # print("avg_price: " + str(lt.total_amount_now / lt.number_now))
 
-    print("---------")
+    print("*************")
 
     lt = LongTrading.LongTrading()
     lt.buy(1000, 1600)
@@ -47,3 +51,79 @@ if __name__ == "__main__":
     print("amount: " + str(lt.total_amount_now))
     print("number: " + str(lt.number_now))
     print("profit: " + str(result[2]))
+
+    print("*************")
+
+    lt = LongTrading.LongTrading()
+    lt.buy(1000, 1600)
+    print("amount: " + str(lt.total_amount_now))
+    print("number: " + str(lt.number_now))
+
+    result = lt.sell(2000, 2500)
+    print("amount: " + str(lt.total_amount_now))
+    print("number: " + str(lt.number_now))
+    print("profit: " + str(result[2]))
+
+def short_trading_test():
+
+    print("short_trading_test")
+
+    st = ShortTrading.ShortTrading()
+    st.short_sell(1000, 2000)
+    print("amount: " + str(st.total_amount_now))
+    print("number: " + str(st.number_now))
+
+    st.short_sell(500, 1500)
+    print("amount: " + str(st.total_amount_now))
+    print("number: " + str(st.number_now))
+
+    result = st.short_cover(500, 1500)
+    print("amount: " + str(st.total_amount_now))
+    print("number: " + str(st.number_now))
+    print("profit: " + str(result[2]))
+
+    result = st.short_cover(1000, 2000)
+    print("amount: " + str(st.total_amount_now))
+    print("number: " + str(st.number_now))
+    print("profit: " + str(result[2]))
+
+    print("*************")
+
+    st = ShortTrading.ShortTrading()
+    st.short_sell(1000, 2000)
+    print("amount: " + str(st.total_amount_now))
+    print("number: " + str(st.number_now))
+
+    st.short_sell(1000, 1000)
+    print("amount: " + str(st.total_amount_now))
+    print("number: " + str(st.number_now))
+
+    result = st.short_cover(1000, 1500)
+    print("amount: " + str(st.total_amount_now))
+    print("number: " + str(st.number_now))
+    print("profit: " + str(result[2]))
+
+    st.short_sell(1000, 1500)
+    print("amount: " + str(st.total_amount_now))
+    print("number: " + str(st.number_now))
+
+    result = st.short_cover(2000, 1000)
+    print("amount: " + str(st.total_amount_now))
+    print("number: " + str(st.number_now))
+    print("profit: " + str(result[2]))
+
+    print("*************")
+
+    st = ShortTrading.ShortTrading()
+    st.short_sell(1000, 2000)
+    print("amount: " + str(st.total_amount_now))
+    print("number: " + str(st.number_now))
+
+    result = st.short_cover(2000, 1500)
+    print("amount: " + str(st.total_amount_now))
+    print("number: " + str(st.number_now))
+    print("profit: " + str(result[2]))
+
+if __name__ == "__main__":
+    long_trading_test()
+    short_trading_test()
---
+++ training/ShortTrading.py
@@ -0,0 +1,55 @@
+class ShortTrading:
+
+    # 空売りによる手持ちの総金額
+    total_amount_now:float
+
+    # 空売りの総株数
+    number_now:int
+
+    def __init__(self) -> None:
+        self.total_amount_now = 0
+        self.number_now = 0
+        pass
+
+    def short_sell(self, selling_number, stock_price):
+        """
+        株を空売りする
+        Args:
+            selling_number (int): 今回空売り株数
+            stock_price (float): 空売り時の株価
+        Returns:
+            float: 今回の引き取りの後、空売りによる手持ちの総金額
+            int:  今回の引き取りの後、空売りした総株数
+        """
+        # 空売り株数が0の場合、実際何も行われない
+        if selling_number == 0:
+            return (0.0, 0)
+
+        self.total_amount_now = self.total_amount_now + selling_number * stock_price
+        self.number_now = self.number_now + selling_number
+
+        return (self.total_amount_now, self.number_now)
+
+    def short_cover(self, short_covering_number, stock_price):
+        """
+        空売りした株を買い戻す
+        Args:
+            short_covering_number (int): 今回買い戻す株数
+            stock_price (float): 買い戻す時の株価
+        Returns:
+            float: 今回買い戻した後、空売りによる手持ちの総金額
+            int: 今回買い戻した後、空売りした総株数
+            float: 損益
+        """
+
+        if short_covering_number > self.number_now:
+            return (0.0, 0, 0.0)
+
+        if short_covering_number == 0:
+            return (0.0, 0, 0.0)
+
+        profit = (self.total_amount_now / self.number_now - stock_price) * short_covering_number
+        self.total_amount_now = self.total_amount_now - short_covering_number * self.total_amount_now / self.number_now
+        self.number_now = self.number_now - short_covering_number
+
+        return (self.total_amount_now, self.number_now, profit)
