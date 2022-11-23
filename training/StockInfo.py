677bcac68f09338fdc87c9b204271b395e678f4d
john_summer <john.midsummer.ac@gmail.com>
2022-11-23 13:58:50 +0900

    fix: 株情報取得処理を修正する(コミット漏れ)

---
+++ training/StockInfo.py
@@ -0,0 +1,30 @@
+from datetime import date
+from datetime import timedelta
+
+import pandas as pd
+import yfinance as yf
+
+
+class StockInfo:
+
+    # 株価格のデータ
+    stock_data_df:pd.DataFrame = None
+
+    # 株銘柄の情報
+    # stock_info = None   # 現時点は使わない
+    code:str
+    start_date:date
+    end_date:date
+
+    def __init__(self, code:str, start_date:date, end_date:date):
+
+        self.code = code
+        self.start_date = start_date
+        self.end_date = end_date
+
+        # yfinanceの仕様的に指定した終了日付の前日までデータを取得してくるので、1日を追加する
+        end_date = end_date + timedelta(days=1)
+
+        start_str = start_date.strftime('%Y-%m-%d')
+        end_str = end_date.strftime('%Y-%m-%d')
+        self.stock_data_df = yf.download(code + '.T', start=start_str, end=end_str, interval = "1d")
