b9e2ad1384aabf85461462299d2657e624e13997
john_summer <john.midsummer.ac@gmail.com>
2022-11-23 16:50:32 +0900

    fix: STASS-128 トレード再開の場合、大引け、寄付それぞれの資産額を指定できるように修正した

--- training/TradingTraining-CLI.py
+++ training/TradingTraining-CLI.py
@@ -41,26 +41,36 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='トレード練習ツール(CLIプロトタイプ版)')
 
     parser.add_argument('code', help='銘柄コード(XXXX)')
-    parser.add_argument('-s', help='トレード開始日付(yyyymmdd)。デフォルトは2018/01/01')
+    parser.add_argument('-s', help='トレード開始日付(yyyymmdd)。デフォルトは2013/01/01')
     parser.add_argument('-e', help='トレード終了日付(yyyymmdd)。デフォルトは実行日付')
     parser.add_argument('-l', help='1ロットの株数。デフォルトは100株')
-    parser.add_argument('-a', help='トレード用の想定金額(初期資産)。デフォルトは1000万(円)')
+    parser.add_argument('-a', help='トレード用の想定金額(初期資産)。デフォルトは1000万(円)。「,」で大引けと寄付注文の資産値をそれぞれ指定できる（トレード再開時に使われる想定）')
 
     args = parser.parse_args()
 
     # 各引数のデフォルト値を定義する
-    start_date_str = '20180101' if args.s == None else args.s
+    start_date_str = '20130101' if args.s == None else args.s
     start_date = dt.datetime.strptime(start_date_str, '%Y%m%d').date()
     end_date = dt.datetime.now().date() if args.e == None else dt.datetime.strptime(args.e, '%Y%m%d').date()
     lot_volumn = 100 if args.l == None else int(args.l)
-    assets = 10000000 if args.a == None else float(args.a)
+
+    assets_close = 10000000
+    assets_open = assets_close
+
+    if args.a != None:
+        assets_input = args.a.split(',')
+        assets_close = float(assets_input[0])
+        if len(assets_input) > 1:
+            assets_open = float(assets_input[1])
+        else:
+            assets_open = assets_close
 
     # 起動時入力された引数に関する動作確認
     print('code=' + args.code)
     print('start_date=' + str(start_date))
     print('end_date=' + str(end_date))
     print('lot=' + str(lot_volumn))
-    print('assets=' + str(assets))
+    print('assets=' + str(assets_close) + ',' + str(assets_open))
 
     print('データ読み込み中。。。')
     stock_info = si.StockInfo(args.code, start_date, end_date)
@@ -74,8 +84,8 @@ if __name__ == "__main__":
     # i = 0
 
     # トレーディングオブジェクトの初期化
-    trading_close = tr.Trading(stock_info, assets, 'close')
-    trading_next_open = tr.Trading(stock_info, assets, 'open')
+    trading_close = tr.Trading(stock_info, assets_close, 'close')
+    trading_next_open = tr.Trading(stock_info, assets_open, 'open')
 
     # 取引入力における日付の年の部分。Noneでなければ設定されているとする。その場合は年の入力を省くことができる。
     trading_date_year:str = None
