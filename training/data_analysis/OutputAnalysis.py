8e2ffb07982a427cd186796ab61e745eabf3c214
john_summer <john.midsummer.ac@gmail.com>
2024-02-11 11:03:18 +0900

    refact: STASS-232 リファクタリングとコメントの微修正

--- training/data_analysis/OutputAnalysis.py
+++ training/data_analysis/OutputAnalysis.py
@@ -3,6 +3,17 @@ import pandas as pd
 # mathをインポートする
 import math
 
+# ロットサイズを計算する関数を定義する
+def __calc_lot_size(x):
+    # 保有株数と空売り中の株数を結合する
+    nums = x["保有株数リスト"] + x["空売り中の株数リスト"]
+    # 0を除く
+    nums = [n for n in nums if n != 0]
+    # 最大公約数を求める
+    gcd = math.gcd(*nums)
+    # 最大公約数を返す
+    return gcd
+
 def aggregate_csv(csv_path:str, output_to_file:bool):
     """
         指定されたトレード履歴のCSVファイルに対して所定の規則で集計する。
@@ -14,6 +25,7 @@ def aggregate_csv(csv_path:str, output_to_file:bool):
         """
     # csvファイルを読み込む
     df = pd.read_csv(csv_path, encoding="shift-jis")
+
     # 銘柄コードと取引日付と保有株数と空売り中の株数と総資産の列だけを抽出する
     df = df[["銘柄コード", "取引日付", "保有株数", "空売り中の株数", "総資産"]]
     # 銘柄コードが変わるかどうかを判定する列を追加する
@@ -26,18 +38,10 @@ def aggregate_csv(csv_path:str, output_to_file:bool):
     df_agg.columns = ["銘柄コード", "開始日", "終了日", "保有株数リスト", "空売り中の株数リスト", "最初の総資産", "最後の総資産"]
     # インデックスをリセットする
     df_agg = df_agg.reset_index(drop=True)
-    # ロットサイズを計算する関数を定義する
-    def calc_lot_size(x):
-        # 保有株数と空売り中の株数を結合する
-        nums = x["保有株数リスト"] + x["空売り中の株数リスト"]
-        # 0を除く
-        nums = [n for n in nums if n != 0]
-        # 最大公約数を求める
-        gcd = math.gcd(*nums)
-        # 最大公約数を返す
-        return gcd
+
     # ロットサイズを計算して列に追加する
-    df_agg["ロットサイズ"] = df_agg.apply(calc_lot_size, axis=1)
+    df_agg["ロットサイズ"] = df_agg.apply(__calc_lot_size, axis=1)
+
     # 行ごとに保有株数と空売り中の株数との合計を計算する
     df["合計株数"] = df["保有株数"] + df["空売り中の株数"]
     # グループ番号ごとに合計株数の最大値を集約する
@@ -50,6 +54,7 @@ def aggregate_csv(csv_path:str, output_to_file:bool):
     df_agg = pd.concat([df_agg, df_max], axis=1)
     # 最大ロット数を計算する
     df_agg["最大ロット数"] = df_agg["合計最大株数"] // df_agg["ロットサイズ"]
+
     # 資産増額を計算する
     df_agg["資産増額"] = df_agg["最後の総資産"] - df_agg["最初の総資産"]
     # 資産増額を通貨のフォーマットで表示する
@@ -59,9 +64,11 @@ def aggregate_csv(csv_path:str, output_to_file:bool):
     # 結果を出力する
     print(df_agg)
 
+    # CSVファイルに出力
     if output_to_file:
+        # CSVには数値として保存したいので、¥を取り除く
         df_agg["資産増額"] = df_agg["資産増額"].str.replace("¥", "")
         df_agg.to_csv(str(csv_path).replace('_history_', '_summary_'), index=False, encoding="shift-jis")
 
-# 例として、sample.csvというファイルを使う
+# テスト用
 # aggregate_csv('output/trading_history_changeable_20000101_20240203112409_opcl.csv')
